21ea85ff3af55283ca15632691f9c3a9a0700f7d
Venkatesh2007 <venkateshbadamdurga@gmail.com>
2025-07-30 14:15:47 +0530

    Fix: bind to 0.0.0.0 and dynamic port

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,6 @@ def hello():
     return "Hello, I'm Badam Venkatesh!"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
